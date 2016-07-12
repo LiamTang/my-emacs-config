9256ec732def7321b337c2ed10d47f77b3deedd9
落花碎影 <dengpeng_jie@126.com>
2016-07-12 16:33:20 +0800

    the last changes before divided into multiple files

--- init.el
+++ init.el
@@ -18,6 +18,8 @@
 		      swiper
 		      counsel
 		      smartparens
+		      ;; --- popwin---
+		      popwin
 		      ;; --- Themes ---
 		      monokai-theme
 		     ) "Default packages")
@@ -54,22 +56,27 @@
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 (global-set-key (kbd "<f6>") 'ivy-resume)
 (global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> l") 'counsel-load-library)
-(global-set-key (kbd "<f1> i") 'counsel-info-lookup-symbol)
-(global-set-key (kbd "<f1> u") 'counsel-unicode-char)
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counqsel-ag)
-(global-set-key (kbd "C-c l") 'counsel-locate)
+;;(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+;;(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+;;(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+;;(global-set-key (kbd "<f1> l") 'counsel-load-library)
+;;(global-set-key (kbd "<f1> i") 'counsel-info-lookup-symbol)
+;;(global-set-key (kbd "<f1> u") 'counsel-unicode-char)
+;;(global-set-key (kbd "C-c g") 'counsel-git)
+;;(global-set-key (kbd "C-c j") 'counsel-git-grep)
+;;(global-set-key (kbd "C-c k") 'counqsel-ag)
+;;(global-set-key (kbd "C-c l") 'counsel-locate)
 ;;(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-(define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+;;(define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
 
 ;; configure for smartparens
 (require 'smartparens-config)
 (smartparens-global-mode t)
+
+;; configure for popwin
+(require 'popwin)
+(popwin-mode 1)
+
 ;; theme management
 (load-theme 'monokai t)
 
@@ -124,6 +131,8 @@
 ;; highlight the current line
 (global-hl-line-mode t)
 
+;; auto load files changed outside the emacs
+(global-auto-revert-mode t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -139,3 +148,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; key binding
+(global-set-key (kbd "C-h C-f") 'find-function)
+(global-set-key (kbd "C-h C-v") 'find-variable)
+(global-set-key (kbd "C-h C-k") 'find-function-on-key)
