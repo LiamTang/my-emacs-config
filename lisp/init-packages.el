2c8f05edb9ebe7d586a5e80c85d1b93a2e2463a3
dpj <dengpeng_jie@126.com>
2016-07-15 17:43:47 +0800

    add configuration for evil-nerd-commenter

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -139,7 +139,25 @@
 
 ;; evil-nerd-commenter setting
 ;; evil-nerd-commenter can be used without evil
-;;
+;; Emacs key bindings
+;;(global-set-key (kbd "M-;") 'evilnc-comment-or-uncomment-lines)
+;; (global-set-key (kbd "C-c l") 'evilnc-quick-comment-or-uncomment-to-the-line)
+;;(global-set-key (kbd "C-c c") 'evilnc-copy-and-comment-lines)
+;; (global-set-key (kbd "C-c p") 'evilnc-comment-or-uncomment-paragraphs)
+
+;; Vim key bindings
+(require 'evil-leader)
+(global-evil-leader-mode)
+(evil-leader/set-key
+  "ci" 'evilnc-comment-or-uncomment-lines
+  "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
+  "ll" 'evilnc-quick-comment-or-uncomment-to-the-line
+  "cc" 'evilnc-copy-and-comment-lines
+  "cp" 'evilnc-comment-or-uncomment-paragraphs
+  "cr" 'comment-or-uncomment-region
+  "cv" 'evilnc-toggle-invert-comment-line-by-line
+  "\\" 'evilnc-comment-operator ; if you prefer backslash key
+)
 
 
 ;; window-numbering setting
